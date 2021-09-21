f76ed06d55747c5a73029ef0c5aa3a9cc41008fb
Nikita Strygin <nikita6@bk.ru>
2021-09-21 09:46:44 +0300

    Improve influxdb env var handling

--- Program.cs
+++ Program.cs
@@ -17,13 +17,31 @@ namespace Overwatcher
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(o =>
                 {
+                    // dokku has a convention of giving the INFLUXDB_URL via environment variables
+                    // so we read it (along with other env vars) and construct a connection string
                     var influxdbUrl = Environment.GetEnvironmentVariable("INFLUXDB_URL");
+                    var influxdbOrganization = Environment.GetEnvironmentVariable("INFLUXDB_ORGANIZATION");
+                    var influxdbBucket = Environment.GetEnvironmentVariable("INFLUXDB_BUCKET");
+                    var influxdbToken = Environment.GetEnvironmentVariable("INFLUXDB_TOKEN");
                     if (influxdbUrl == null) return;
+                    var builder = new UriBuilder(influxdbUrl);
+                    
+                    var queryString = System.Web.HttpUtility.ParseQueryString(builder.Query);
+
+                    if (influxdbOrganization != null)
+                        queryString.Add("org", influxdbOrganization);
+                    if (influxdbBucket != null)
+                        queryString.Add("bucket", influxdbBucket);
+                    if (influxdbToken != null)
+                        queryString.Add("token", influxdbToken);
+                    
+                    builder.Query = queryString.ToString();
+
                     var memorySource = new MemoryConfigurationSource
                     {
                         InitialData = new[]
                         {
-                            new KeyValuePair<string, string>("ConnectionStrings:SensorInfluxDB", influxdbUrl)
+                            new KeyValuePair<string, string>("ConnectionStrings:SensorInfluxDB", builder.ToString())
                         }
                     };
                     o.Add(memorySource);
